fe1d784a1567a04809671b981dad7f0eeabbcd32
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-04-13 08:48:48 +0200

    Reading & dispatching from a new slice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
-import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -10,6 +12,7 @@ function App() {
     <>
       <Header />
       {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </>
   );
